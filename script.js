6ee4965bc37f2f7c91aa183475579ba0ec2038bc
Peter Leite <peterleite.dev@gmail.com>
2021-09-20 17:58:16 -0300

    funções de ganho de ponto

--- script.js
+++ script.js
@@ -3,6 +3,16 @@ const numberBtn = document.querySelectorAll('.numberBtn')
 
 const benderNumber = parseInt(Math.random() * 11)
 
+const userWon =()=>{
+    console.log('usuario ganhou ' + benderNumber)
+}
+const benderWonRound =()=>{
+    console.log('Bender ganhou o round ' + benderNumber)
+}
 numberBtn.forEach(el => el.addEventListener('click', ()=> {
-    console.log(el.value)
+    if(el.value == benderNumber){
+        userWon()
+    } else {
+        benderWonRound()
+    }
 }))
